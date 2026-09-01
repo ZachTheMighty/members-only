3506e68ae5f74366b52e035cd28b3981ec3de7cd
ZachTheMighty <promoted66@gmail.com>
2026-09-01 04:15:30 +0300

    add: database pool

---
+++ db/pool.js
@@ -0,0 +1,16 @@
+const { Pool } = require("pg");
+const { loadEnvFile } = require("node:process");
+
+try {
+  loadEnvFile();
+} catch (error) {
+  if (error.code !== "ENOENT") throw error;
+}
+
+const ENV = process.env;
+
+module.exports = new Pool({
+  connectionString:
+    ENV.DATABASE_URL ||
+    `postgresql://${ENV.USER}:${ENV.PASSWORD}@${ENV.HOST}:${ENV.DATABASE_SERVER_PORT}/${ENV.DATABASE}`,
+});
