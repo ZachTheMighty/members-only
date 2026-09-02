30ce76c5f93c3e768573d71134a77a9161ca1482
ZachTheMighty <promoted66@gmail.com>
2026-09-02 06:41:53 +0300

    add: constraints so that members see author and date, non members see message and date

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -2,7 +2,6 @@ const db = require("../db/queries.js");
 
 const dashboardGet = (req, res) => {
   res.locals.fullName = `${req.user.first_name} ${req.user.last_name}`;
-  res.locals.isMember = req.user.membership_status;
   res.render("dashboard.ejs");
 };
 
--- controllers/homepageController.js
+++ controllers/homepageController.js
@@ -1,6 +1,7 @@
 const db = require("../db/queries.js");
 
 const homepageGet = async (req, res) => {
+  res.locals.isMember = req.user?.membership_status;
   const messages = await db.getAllMessages();
   res.render("index.ejs", { messages });
 };
