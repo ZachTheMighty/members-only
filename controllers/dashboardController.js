fc9c423387fde98bba498a155919c98aa681f4e6
ZachTheMighty <promoted66@gmail.com>
2026-09-02 04:46:05 +0300

    fix: child routes for dashboard still accessible even after logout
    fixed by putting req.isAuthenticated() middleware at the top of dashboard routes so it executes for every request

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,12 +1,9 @@
 const db = require("../db/queries.js");
 
 const dashboardGet = (req, res) => {
-  if (req.isAuthenticated())
-    res.render("dashboard.ejs", {
-      fullName: `${req.user.first_name} ${req.user.last_name}`,
-    });
-  else
-    res.status(401).json({ msg: "You're not authorized to visit this page" });
+  res.render("dashboard.ejs", {
+    fullName: `${req.user.first_name} ${req.user.last_name}`,
+  });
 };
 
 const createMessageGet = (req, res) => res.render("createMessageForm.ejs");
--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -3,6 +3,13 @@ const route = Router();
 
 const controller = require("../controllers/dashboardController.js");
 
+route.use((req, res, next) => {
+  if (req.isAuthenticated()) next();
+  else
+    res
+      .status(401)
+      .json({ msg: "You are not authorized to view this resource." });
+});
 route.get("/", controller.dashboardGet);
 route.get("/new_message", controller.createMessageGet);
 route.post("/new_message", controller.createMessagePost);
